5813b284336d600e7950be7044d052fb4d9c5739
Erasmo de Melo Gusmão <erasmo.gusmao@gmail.com>
2015-12-25 18:16:09 -0300

    Correção da classe IndicadorMACD - Corrigido início da lista do campo ListaDoMACD, ListaDoSinal;

--- FormatarHistóricoCotações/FormatarHistóricoCotações/IndicadorMACD.cs
+++ FormatarHistóricoCotações/FormatarHistóricoCotações/IndicadorMACD.cs
@@ -15,6 +15,7 @@ namespace FormatarHistóricoCotações
     class IndicadorMACD
     {
         public List<double> ListaDaMACD { get; private set;}
+        private List<double> listaTempMACD;                         //Para calculo da ListaDoSinalMACD
         public List<double> ListaDoSinalMACD { get; private set;}
         public List<double> HistográmaMACD {get; private set;}
 
@@ -29,23 +30,40 @@ namespace FormatarHistóricoCotações
         {
 
             ListaDaMACD = new List<double>();
+            listaTempMACD = new List<double>();
             ListaDoSinalMACD = new List<double>();
 
             ListaDaMACD.Clear();
             ListaDoSinalMACD.Clear();
+            listaTempMACD.Clear();
 
 
             MédiaMóvelExponencial MME_Lenta = new MédiaMóvelExponencial(preçoFechamento, períodoLongo);
             MédiaMóvelExponencial MME_Rápida = new MédiaMóvelExponencial(preçoFechamento, períodoCurto);
 
-            for (int i = 0; i < MME_Lenta.ListaDaMME.Count; i++)
-            {
+            for (int i = 0; i < períodoLongo-1; i++)
+            {// Zero so primeiros elementos
+                ListaDaMACD.Add(0);  
+            }
+
+            for (int i = períodoLongo-1; i < MME_Lenta.ListaDaMME.Count; i++)
+            {//Calcula ListaDaMACD
                 ListaDaMACD.Add(MME_Rápida.ListaDaMME[i] - MME_Lenta.ListaDaMME[i]);
             }
 
-            MédiaMóvelExponencial MME_Sinal = new MédiaMóvelExponencial(ListaDaMACD, períodoSinal);
-            for (int i = 1; i < MME_Sinal.ListaDaMME.Count; i++) //Varre HistóricoLinhaMACD do segundo elemento válido
+            for (int i = períodoLongo-1; i < ListaDaMACD.Count; i++)
+            {//Pega os elementos da ListaDaMACD para calculo do sinal
+                listaTempMACD.Add(ListaDaMACD[i]);
+            }
+           
+            MédiaMóvelExponencial MME_Sinal = new MédiaMóvelExponencial(listaTempMACD, períodoSinal);
+            for (int i = 0; i < períodoLongo-1; i++)
             {
+                ListaDoSinalMACD.Add(0);
+            }
+
+            for (int i = 0; i < MME_Sinal.ListaDaMME.Count; i++)
+            {//Calcula ListaDoSinalMACD
                 ListaDoSinalMACD.Add(MME_Sinal.ListaDaMME[i]);
             }
         }
